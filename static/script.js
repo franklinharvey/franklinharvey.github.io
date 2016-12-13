2a8f7274d39e05e046dd024bc218033c46157054
Franklin Harvey <Franklinadamsharvey@gmail.com>
2016-12-13 10:59:59 -0700

    Added Mailing List Sign Up

--- static/script.js
+++ static/script.js
@@ -1,3 +1,56 @@
-(function () {
-    
-});
+(function() {
+    // Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyDRgrapo6aaWk4_rgwDfqEwFj7KtR8GOVk",
+	    authDomain: "personal-site-1e6e3.firebaseapp.com",
+	    databaseURL: "https://personal-site-1e6e3.firebaseio.com",
+	    storageBucket: "personal-site-1e6e3.appspot.com",
+	    messagingSenderId: "1063535386214"
+  	};
+	console.log("IN SCRIPT");
+	firebase.initializeApp(config);
+
+	const txtEmail = document.getElementById('txtEmail');
+	const txtFirstName = document.getElementById('txtFirstName');
+	const txtLastName = document.getElementById('txtLastName');
+	const btnSignUp = document.getElementById('btnSignUp');
+
+	btnSignUp.addEventListener('click', e => {
+		console.log("IN SIGN UP");
+		const email = txtEmail.value;
+		const firstName = txtFirstName.value;
+		const lastName = txtLastName.value;
+		if (email == "") {
+			alert("Please enter an email address");
+			return;
+		}
+		if (firstName == "") {
+			alert("Please enter an email address");
+			return;
+		}
+		if (lastName == "") {
+			alert("Please enter an email address");
+			return;
+		}
+		console.log("BEFORE SAVE EVENT");
+		saveUser(email,firstName,lastName);
+		clearField(txtEmail,txtFirstName,txtLastName);
+	});
+
+	function saveUser(email,firstName,lastName) {
+		console.log("IN SAVE USER");
+		const entry = firstName+lastName;
+		firebase.database().ref("EmailList/" + entry).set({
+			FirstName : firstName,
+			LastName : lastName,
+			Email : email
+		});
+	};
+	function clearField(txtEmail,txtFirstName,txtLastName) {
+    	console.log("IN CLEAR FIELD");
+    	txtEmail.value = "";
+    	txtFirstName.value = "";
+    	txtLastName.value = "";
+	};
+
+}());
